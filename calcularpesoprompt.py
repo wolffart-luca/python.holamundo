6837107e6da0aae9bb9d4d8d5bf7330b62e17d0a
wolffart-luca <128066194+wolffart-luca@users.noreply.github.com>
2023-03-25 13:10:00 -0300

    Add files via upload
    Preguntas via inputs, luego responder matematicas simples

---
+++ calcularpesoprompt.py
@@ -0,0 +1,5 @@
+prompt1 = float(input("hola, comentame cuanto pesas. "))
+prompt2 = float(input("Ahora dime cuanto mides "))
+
+resp = (prompt1 / (prompt2*2))
+print("tu masa corporal es de ", resp)
